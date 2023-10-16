4cde925af50038eecfe2a20e141c5a0fb1e162b2
Oscar7634E <148064586+Oscar7634E@users.noreply.github.com>
2023-10-16 13:15:48 +0100

    Change the title of the window

--- Main.py
+++ Main.py
@@ -1,10 +1,13 @@
 import pygame
 
 pygame.init() # initialaising pygame 
-screen = pygame.display.set_mode((1000,500)) #creates the surface
+screen = pygame.display.set_mode((1000,500)) #creates the surface, 1000 = width, 500 = height
+pygame.display.set_caption("Frunace Man") # Changes ther name of the window
 running = True # Makes the 
 while True: # makes the previous code continue to run
     for event in pygame.event.get():
         if event.type == pygame.QUIT: # is equal to the "x" button on the window
             pygame.quit() # opposite of pygame.init
-    pygame.display.update()
+            exit() # cancels the "while True"
+
+    pygame.display.update() 
