2720cb869d1628143547dc74f5f0f4875298d16a
TibsT1 <tiberiumtoma@gmail.com>
2023-10-17 12:48:37 +0100

    I am about half way into fixing the shooting projectile
    I tried expanding on the projectile class a little bit. I have changed the sprite for the player and created my own sprite for the fireball. I tried making it so that the projectiles get added to an array but it doesn't work. I will attempt to fix it later.

--- Main.py
+++ Main.py
@@ -18,20 +18,37 @@ screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 background = pygame.image.load("Images/bg_spaceship_1.png")
 
 # Creates the player
-player = Player(370, 100, pygame.image.load("Images/FurnaceMan.png"), 250, 500)
-
-# Creates the projectile
-projectile_left = Projectile(410, 410, pygame.image.load("Images/FB001_Left.png"), 150, 75)
+player = Player(370, 130, pygame.image.load("Images/FurnaceMan.png"), 250, 400)
 
 # Changes the name of the window
 pygame.display.set_caption("Furnace Man")
+
+fireballs = []
 
 # Keeps the game running
 running = True 
 while running:
 
+    for fireball in fireballs[::-1]:
+        if fireball.x < 1000 and fireball.x > 0:
+            fireball.x += fireball.vel
+        else:
+            fireballs.pop(fireballs.index(fireball))
+
+    keys = pygame.key.get_pressed()
+
+
+    # Key press creates fireball
+    if keys[pygame.K_RIGHT]:
+        if len(fireballs) < 1:
+            fireballs.append(Projectile(470, 380, pygame.image.load("Images/Fireball.png"), 100, 70))
+            for fireball in fireballs:
+                fireball.draw(screen)
+        else:
+            continue
+
     # Sets the frame rate
-    clock.tick(60)
+    clock.tick(30)
 
     for event in pygame.event.get():
 
@@ -40,13 +57,6 @@ while running:
 
         # Draws the player
         screen.blit(player.img, (player.x, player.y))
-
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                while projectile_left.x > 0:
-                    screen.blit(projectile_left.img, (projectile_left.x, projectile_left.y))
-                    projectile_left.x -= projectile_left.vel
-
 
         # Quits the game
         if event.type == pygame.QUIT:
